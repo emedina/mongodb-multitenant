748f6d18f45a54a1457cfe9a68b56d6b73e46d2f
Per Sundberg <per.martin.sundberg@gmail.com>
2011-03-16 18:26:21 +0100

    adding tenant handling

---
+++ src/groovy/se/webinventions/mongomultitenant/MongoTenantDatastoreFactoryBean.groovy
@@ -0,0 +1,11 @@
+package se.webinventions.mongomultitenant
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: per
+ * Date: 2011-03-12
+ * Time: 13:25
+ * To change this template use File | Settings | File Templates.
+ */
+class MongoTenantDatastoreFactoryBean {
+}
