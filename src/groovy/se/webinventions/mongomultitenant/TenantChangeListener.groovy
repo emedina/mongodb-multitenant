79d2fc15203fe4557eeebee36dc85a460f4e48ab
Per <contact@webinventions.se>
2011-05-05 11:41:39 +0200

    added some change listener for tenant changes

---
+++ src/groovy/se/webinventions/mongomultitenant/TenantChangeListener.groovy
@@ -0,0 +1,12 @@
+package se.webinventions.mongomultitenant
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: per
+ * Date: 2011-05-03
+ * Time: 12:53
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TenantChangeListener {
+
+}
