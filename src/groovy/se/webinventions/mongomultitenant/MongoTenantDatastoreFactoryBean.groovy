c1a10c266fc8b2f724e8b1f68adf67d6a1b75f19
Enrique Medina Montenegro <enrique@medinamontenegro.es>
2011-10-14 11:14:49 +0300

    More updates to make it compatible with latest MongoDB plugin v1.0.0.M7

--- src/groovy/se/webinventions/mongomultitenant/MongoTenantDatastoreFactoryBean.groovy
+++ src/groovy/se/webinventions/mongomultitenant/MongoTenantDatastoreFactoryBean.groovy
@@ -2,13 +2,16 @@
 
 package se.webinventions.mongomultitenant
 
-import org.grails.datastore.gorm.events.AutoTimestampInterceptor 
-import org.grails.datastore.gorm.events.DomainEventInterceptor 
-import org.springframework.beans.factory.FactoryBean;
-import org.springframework.datastore.mapping.model.MappingContext;
-import org.springframework.datastore.mapping.mongo.MongoDatastore;
+
 
 import com.mongodb.Mongo;
+import org.grails.datastore.gorm.events.AutoTimestampEventListener
+import org.grails.datastore.gorm.events.DomainEventListener
+import org.springframework.beans.factory.FactoryBean
+import org.springframework.context.ApplicationContext
+
+import org.grails.datastore.mapping.model.MappingContext
+import org.springframework.context.ApplicationContextAware
 
 /**
  * Factory bean for constructing a {@link MongoTenantDatastore} instance.
@@ -16,26 +19,29 @@ import com.mongodb.Mongo;
  * @author Per Sundberg
  *
  */
-class MongoTenantDatastoreFactoryBean implements FactoryBean<MongoTenantDatastore>{
+class MongoTenantDatastoreFactoryBean implements FactoryBean<MongoTenantDatastore>,  ApplicationContextAware{
 
 	Mongo mongo
 	MappingContext mappingContext
 	Map<String,String> config = [:]
   MongodbTenantResolver tenantResolverProxy
   MongoTenantDatastore datastore
+   ApplicationContext applicationContext
 
 	@Override
 	public MongoTenantDatastore getObject() throws Exception {
 		
 		if(!datastore) {
       if(mongo != null)
-             datastore = new MongoTenantDatastore(mappingContext, mongo,config,tenantResolverProxy)
+             datastore = new MongoTenantDatastore(mappingContext, mongo,config,applicationContext)
           else {
-            datastore = new MongoTenantDatastore(mappingContext, config,tenantResolverProxy)
+            datastore = new MongoTenantDatastore(mappingContext, config,applicationContext)
           }
 
-          datastore.addEntityInterceptor(new DomainEventInterceptor())
-          datastore.addEntityInterceptor(new AutoTimestampInterceptor())
+      datastore.setTenantResolverProxy(tenantResolverProxy)
+
+             applicationContext.addApplicationListener(new AutoTimestampEventListener(datastore))
+        applicationContext.addApplicationListener(new DomainEventListener(datastore))
           datastore.afterPropertiesSet()
 
     }
